d84ee41c2b4129de11d18809051e19d64816298b
Tyrov <dakingxvi@gmail.com>
2018-12-18 00:06:36 -0500

    oops

--- elevator.py
+++ elevator.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-with open("token.txt") as token_file
+with open("token.txt") as token_file:
     TOKEN = token_file.read().strip()
 
 client = commands.Bot(command_prefix='.')
