b477cbadb13785fbf935c582e3c361362dea65b4
Paul Teter <pteter20@stjohnschs.org>
2018-12-18 01:42:24 -0500

    Added chat log functionality

--- elevator.py
+++ elevator.py
@@ -10,4 +10,20 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print("Initialized")
 
+@client.command()
+async def ping():
+    await client.say("Pong!")
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    if author != client.user:
+        try:
+            channel = discord.utils.get(author.server.channels, name="chat-logs")
+            await client.send_message(channel, "{}: {}".format(author, content))
+            await client.process_commands(message)
+        except:
+            print("Message unable to be logged")
+
 client.run(TOKEN)
